6f612163cb899a61f52d7d0dbb9932c7492dd12c
Fergus Bremner <fergus.bremner@gmail.com>
2014-03-06 14:04:09 +0100

    added nocache

--- lib/git-prompt.zsh
+++ lib/git-prompt.zsh
@@ -25,6 +25,7 @@ function preexec_update_git_vars() {
 }
 
 function precmd_update_git_vars() {
+    ZSH_THEME_GIT_PROMPT_NOCACHE=1
     if [ -n "$__EXECUTED_GIT_COMMAND" ] || [ -n "$ZSH_THEME_GIT_PROMPT_NOCACHE" ]; then
         update_current_git_vars
         unset __EXECUTED_GIT_COMMAND
@@ -84,9 +85,9 @@ ZSH_THEME_GIT_PROMPT_PREFIX="["
 ZSH_THEME_GIT_PROMPT_SUFFIX="]"
 ZSH_THEME_GIT_PROMPT_SEPARATOR="|"
 ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg[yellow]%}"
-ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[red]%}●"
+ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[green]%}●"
 ZSH_THEME_GIT_PROMPT_CONFLICTS="%{$fg[red]%}×"
 ZSH_THEME_GIT_PROMPT_CHANGED="%{$fg[blue]%}+"
 ZSH_THEME_GIT_PROMPT_REMOTE=""
 ZSH_THEME_GIT_PROMPT_UNTRACKED="…"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[green]%}✔ "
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[white]%}✔ "
