313efa14ad835f4b2e9932c4ccb1b34dee403497
Fergus Bremner <fergus.bremner@gmail.com>
2014-03-07 00:29:54 +0100

    prompt test 2

--- lib/git-prompt.zsh
+++ lib/git-prompt.zsh
@@ -1,12 +1,12 @@
-# To install source this file from your .zshrc file
+#!/usr/bin/env zsh
 
-# Change this to reflect your installation directory
+# change this to reflect your installation directory
 export __GIT_PROMPT_DIR=~/.zsh/scripts
-# Initialize colors.
-autoload -U colors
-colors
 
-# Allow for functions in the prompt.
+# initialize colors.
+autoload -U colors && colors
+
+# allow for functions in the prompt.
 setopt PROMPT_SUBST
 
 autoload -U add-zsh-hook
@@ -25,6 +25,7 @@ function preexec_update_git_vars() {
 }
 
 function precmd_update_git_vars() {
+    ZSH_THEME_GIT_PROMPT_NOCACHE=1
     if [ -n "$__EXECUTED_GIT_COMMAND" ] || [ -n "$ZSH_THEME_GIT_PROMPT_NOCACHE" ]; then
         update_current_git_vars
         unset __EXECUTED_GIT_COMMAND
@@ -35,6 +36,16 @@ function chpwd_update_git_vars() {
     update_current_git_vars
 }
 
+# show count of stashed changes
+function parse_git_stash() {
+  local -a stashes
+
+  if [[ -s $(git rev-parse --show-toplevel)/.git/refs/stash ]]; then
+    stashes=$(git stash list 2> /dev/null | wc -l | tr -d ' ')
+    echo "[${stashes} stashed]"
+  fi
+}
+ 
 function update_current_git_vars() {
     unset __CURRENT_GIT_STATUS
 
@@ -50,15 +61,16 @@ function update_current_git_vars() {
 	GIT_CLEAN=$__CURRENT_GIT_STATUS[7]
 }
 
-
 git_super_status() {
 	precmd_update_git_vars
+    BRANCH_STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX$ZSH_THEME_GIT_PROMPT_BRANCH$GIT_BRANCH%{${reset_color}%}$ZSH_THEME_GIT_PROMPT_SUFFIX"
     if [ -n "$__CURRENT_GIT_STATUS" ]; then
-	  STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX$ZSH_THEME_GIT_PROMPT_BRANCH$GIT_BRANCH%{${reset_color}%}"
+    #BRANCH_STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX$ZSH_THEME_GIT_PROMPT_BRANCH$GIT_BRANCH%{${reset_color}%}$ZSH_THEME_GIT_PROMPT_SUFFIX"
+	  STATUS="$BRANCH_STATUS"
 	  if [ -n "$GIT_REMOTE" ]; then
-		  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_REMOTE$GIT_REMOTE%{${reset_color}%}"
+		  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_PREFIX$ZSH_THEME_GIT_PROMPT_REMOTE$GIT_REMOTE%{${reset_color}%}$ZSH_THEME_GIT_PROMPT_SUFFIX"
 	  fi
-	  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_SEPARATOR"
+	  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_PREFIX"
 	  if [ "$GIT_STAGED" -ne "0" ]; then
 		  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_STAGED$GIT_STAGED%{${reset_color}%}"
 	  fi
@@ -71,25 +83,20 @@ git_super_status() {
 	  if [ "$GIT_UNTRACKED" -ne "0" ]; then
 		  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_UNTRACKED%{${reset_color}%}"
 	  fi
-	  if [ "$GIT_CLEAN" -eq "1" ]; then
-		  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CLEAN"
-	  fi
 	  STATUS="$STATUS%{${reset_color}%}$ZSH_THEME_GIT_PROMPT_SUFFIX"
-	  echo "$STATUS"
+	  if [ "$GIT_CLEAN" -eq "1" ]; then
+      STATUS="$BRANCH_STATUS"
+	  fi
+    echo "$STATUS$(parse_git_stash)"
 	fi
 }
 
-# Default values for the appearance of the prompt. Configure at will.
-ZSH_THEME_GIT_PROMPT_PREFIX="("
-ZSH_THEME_GIT_PROMPT_SUFFIX=")"
-ZSH_THEME_GIT_PROMPT_SEPARATOR="|"
-ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg_bold[magenta]%}"
-ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[red]%}●"
-ZSH_THEME_GIT_PROMPT_CONFLICTS="%{$fg[red]%}✖"
-ZSH_THEME_GIT_PROMPT_CHANGED="%{$fg[blue]%}✚"
-ZSH_THEME_GIT_PROMPT_REMOTE=""
+# default values for the appearance of the prompt
+ZSH_THEME_GIT_PROMPT_PREFIX="["
+ZSH_THEME_GIT_PROMPT_SUFFIX="]"
+ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg[yellow]%}"
+ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[green]%}+"
+ZSH_THEME_GIT_PROMPT_CONFLICTS="%{$fg[red]%}×"
+ZSH_THEME_GIT_PROMPT_CHANGED="%{$fg[magenta]%}+"
+ZSH_THEME_GIT_PROMPT_REMOTE="%{$fg[blue]%}"
 ZSH_THEME_GIT_PROMPT_UNTRACKED="…"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}✔"
-
-
-
