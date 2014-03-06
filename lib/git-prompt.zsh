4b2580338a83753e98fa962687aeaf4c39a0b1ee
Fergus Bremner <fergus.bremner@gmail.com>
2014-03-06 19:32:20 +0100

    new staged symbol

--- lib/git-prompt.zsh
+++ lib/git-prompt.zsh
@@ -1,12 +1,12 @@
 # To install source this file from your .zshrc file
 
-# Change this to reflect your installation directory
+# change this to reflect your installation directory
 export __GIT_PROMPT_DIR=~/.zsh/scripts
 # Initialize colors.
 autoload -U colors
 colors
 
-# Allow for functions in the prompt.
+# allow for functions in the prompt.
 setopt PROMPT_SUBST
 
 autoload -U add-zsh-hook
@@ -95,9 +95,9 @@ ZSH_THEME_GIT_PROMPT_PREFIX="["
 ZSH_THEME_GIT_PROMPT_SUFFIX="]"
 ZSH_THEME_GIT_PROMPT_SEPARATOR="|"
 ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg[yellow]%}"
-ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[green]%}●"
+ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[green]%}±"
 ZSH_THEME_GIT_PROMPT_CONFLICTS="%{$fg[red]%}×"
 ZSH_THEME_GIT_PROMPT_CHANGED="%{$fg[blue]%}+"
-ZSH_THEME_GIT_PROMPT_REMOTE=""
+ZSH_THEME_GIT_PROMPT_REMOTE="∓"
 ZSH_THEME_GIT_PROMPT_UNTRACKED="…"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[white]%}✔ "
