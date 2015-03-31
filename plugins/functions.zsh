405c86c48b9f22887ab91b9d3125e7accd34a475
Fergus Bremner <fergus.bremner@gmail.com>
2015-03-31 09:22:32 -0400

    added up function

--- plugins/functions.zsh
+++ plugins/functions.zsh
@@ -23,6 +23,25 @@ qfind () {
     return 0
 }
 
+# cd quickly with an up command
+up() {
+  local op=print
+  [[ -t 1 ]] && op=cd
+  case "$1" in
+    '') up 1;;
+    -*|+*) $op ~$1;;
+    <->) $op $(printf '../%.0s' {1..$1});;
+    *) local -a seg; seg=(${(s:/:)PWD%/*})
+       local n=${(j:/:)seg[1,(I)$1*]}
+       if [[ -n $n ]]; then
+         $op /$n
+       else
+         print -u2 up: could not find prefix $1 in $PWD
+         return 1
+       fi
+  esac
+}
+
 # extract depending on extension
 extract() {
   emulate -L zsh
