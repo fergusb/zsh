7a5b8f106299deb9566527d8b2ed9f0758c83826
Fergus Bremner <fergus.bremner@gmail.com>
2017-03-17 13:09:54 +0100

    more testing on ahead / behind prompt

--- scripts/zshrc.sh
+++ scripts/zshrc.sh
@@ -92,7 +92,20 @@ git_super_status() {
 	fi
 }
 
-# Default values for the appearance of the prompt. Configure at will.
+# Default values for the appearance of the prompt
+# ZSH_THEME_GIT_PROMPT_PREFIX="("
+# ZSH_THEME_GIT_PROMPT_SUFFIX=")"
+# ZSH_THEME_GIT_PROMPT_SEPARATOR="|"
+# ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg_bold[magenta]%}"
+# ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[red]%}%{●%G%}"
+# ZSH_THEME_GIT_PROMPT_CONFLICTS="%{$fg[red]%}%{✖%G%}"
+# ZSH_THEME_GIT_PROMPT_CHANGED="%{$fg[blue]%}%{✚%G%}"
+# ZSH_THEME_GIT_PROMPT_BEHIND="%{↓%G%}"
+# ZSH_THEME_GIT_PROMPT_AHEAD="%{↑%G%}"
+# ZSH_THEME_GIT_PROMPT_UNTRACKED="%{…%G%}"
+# ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}%{✔%G%}"
+
+# Custom values 
 ZSH_THEME_GIT_PROMPT_PREFIX="["
 ZSH_THEME_GIT_PROMPT_SUFFIX="]"
 ZSH_THEME_GIT_PROMPT_SEPARATOR="|"
@@ -100,8 +113,8 @@ ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg[yellow]%}"
 ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[green]%}%{+%G%}"
 ZSH_THEME_GIT_PROMPT_CONFLICTS="%{$fg[red]%}%{×%G%}"
 ZSH_THEME_GIT_PROMPT_CHANGED="%{$fg[magenta]%}%{+%G%}"
-ZSH_THEME_GIT_PROMPT_BEHIND="%{$fg[blue]%↓%G%}"
-ZSH_THEME_GIT_PROMPT_AHEAD="%{$fg[blue]%↑%G%}"
+ZSH_THEME_GIT_PROMPT_BEHIND="%{↓%G%}"
+ZSH_THEME_GIT_PROMPT_AHEAD="%{↑%G%}"
 ZSH_THEME_GIT_PROMPT_UNTRACKED="%{…%G%}"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[green]%}%{✔%G%}"
 
